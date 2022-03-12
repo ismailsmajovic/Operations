dec0ee7466bd03f366cac616209a2f165a2ed047
Ismail <Ismail@DESKTOP-G0GSDUM>
2022-03-12 19:12:42 +0100

    init

---
+++ add.js
@@ -0,0 +1,16 @@
+function racunaj(){
+    var b1=parseInt(document.getElementById("b1").value);
+    var b2=parseInt(document.getElementById("b2").value);
+    var sum = b1+b2;
+    document.getElementById("b3").innerHTML=sum;
+}
+
+/*var dugme = document.getElementbyClass("dugme");
+
+dugme.addEventListener("click", racunaj());*/ 
+function racunaj2(){
+    var b4=parseInt(document.getElementById("b4").value);
+    var b5=parseInt(document.getElementById("b5").value);
+    var mul = b4 * b5;
+    document.getElementById("b6").innerHTML=mul;
+}
