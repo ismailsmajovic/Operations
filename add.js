5be146baa30abceca02a19b421b0894ae7a67d62
Ismail <Ismail@DESKTOP-G0GSDUM>
2022-04-05 16:49:51 +0200

    init

--- add.js
+++ add.js
@@ -3,8 +3,15 @@ function racunaj(){
     var b2=parseInt(document.getElementById("b2").value);
     var sum = b1+b2;
     document.getElementById("b3").innerHTML=sum;
-}
 
+    if(sum > 5){
+        alert("good");
+        return false;
+    }
+    else{
+        alert("LOSE");
+    }
+}
 /*var dugme = document.getElementbyClass("dugme");
 
 dugme.addEventListener("click", racunaj());*/ 
@@ -13,4 +20,12 @@ function racunaj2(){
     var b5=parseInt(document.getElementById("b5").value);
     var mul = b4 * b5;
     document.getElementById("b6").innerHTML=mul;
+
+    if(mul > 50){
+        alert("good");
+        return false;
+    }
+    else{
+       document.write("<h1>Bezao si sa matematike</h1>");
+    }
 }
